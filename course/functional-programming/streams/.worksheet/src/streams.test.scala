a35396d82680acfc943d2250b1805a5b815f2ba3
Fernando Benjamin <benibadboy@hotmail.com>
2014-09-23 12:22:10 +0200

    go

---
+++ course/functional-programming/streams/.worksheet/src/streams.test.scala
@@ -0,0 +1,7 @@
+package streams
+
+object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(74); 
+  println("Welcome to the Scala worksheet");$skip(20); 
+  val a=List(1,2,3);System.out.println("""a  : List[Int] = """ + $show(a ));$skip(7); val res$0 = 
+  5::a;System.out.println("""res0: List[Int] = """ + $show(res$0))}
+}
---
+++ course/functional-programming/streams/src/main/scala/streams/test.sc
@@ -0,0 +1,7 @@
+package streams
+
+object test {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  val a=List(1,2,3)                               //> a  : List[Int] = List(1, 2, 3)
+  5::a                                            //> res0: List[Int] = List(5, 1, 2, 3)
+}
