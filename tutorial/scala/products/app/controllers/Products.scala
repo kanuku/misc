5d02bb0d38cc9d64c9ec93b8f90ce54e21a07899
Fernando Benjamin <fernando.benjamin@zalando.de>
2014-12-04 17:35:01 +0100

    Page 88 of chapter 4

--- tutorial/scala/products/app/controllers/Products.scala
+++ tutorial/scala/products/app/controllers/Products.scala
@@ -8,44 +8,15 @@ import models.Product
 import play.api.mvc.Flash
 
 object Products extends Controller {
-  private val productForm: Form[Product] = Form(
-    mapping(
-      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
-      "name" -> nonEmptyText,
-      "description" -> nonEmptyText)(Product.apply)(Product.unapply))
-
-  def list = Action { implicit request =>
-    val products = Product.findAll
-    Ok(views.html.products.list(products))
-  }
-
-  def show(ean: Long) = Action { implicit request =>
-    Product.findByEan(ean).map(
-      product => Ok(views.html.products.details(product))).getOrElse(NotFound)
-  }
-
-  def save = Action { implicit request =>
-    val newProductForm = productForm.bindFromRequest()
-    newProductForm.fold(
-      hasErrors = { form =>
-        Redirect(routes.Products.newProduct()).flashing(Flash(form.data) +
-          ("error" -> Messages("validation.errors")))
-      },
-      success = { newProduct =>
-        Product.add(newProduct)
-        val message = Messages("products.new.success", newProduct.name)
-        Redirect(routes.Products.show(newProduct.ean))
-          .flashing("success" -> message)
-      })
-  }
-
-  def newProduct = Action {
-    implicit request =>
-      val form = if (request2flash.get("error").isDefined)
-        productForm.bind(request2flash.data)
-      else
-        productForm
-      Ok(views.html.products.editProduct(form))
-  }
-
+  def list(pageNumber: Int) = Action {
+    NotImplemented
 }
+  def details(ean: Long) = Action {
+    NotImplemented
+}
+  def edit(ean: Long) = Action {
+    NotImplemented
+}
+  def update(ean: Long) = Action {
+    NotImplemented
+} }
