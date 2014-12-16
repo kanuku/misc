b00ae092bf0a3e7efa8dc498d6cecb18d0dd4909
Fernando Benjamin <fernando.benjamin@zalando.de>
2014-12-16 12:30:33 +0100

    Building Multirecord Parser

--- tutorial/scala/products/app/models/Product.scala
+++ tutorial/scala/products/app/models/Product.scala
@@ -36,4 +36,28 @@ object Product {
           Product(id, ean, name, description)
       }.toList
   }
+
+  import anorm.RowParser
+  val productParser: RowParser[Product] = {
+    import anorm.~
+    import anorm.SqlParser._
+    long("id") ~
+      long("ean") ~
+      str("name") ~
+      str("description") map {
+        case id ~ ean ~ name ~ description =>
+          Product(id, ean, name, description)
+      }
+  }
+
+  import anorm.ResultSetParser
+  val productsParser: ResultSetParser[List[Product]] = {
+    productParser *
+  }
+
+  def getAllWithParser: List[Product] = DB.withConnection {
+    implicit connection =>
+      sql.as(productsParser)
+  }
+
 }
--- tutorial/scala/products/build.sbt
+++ tutorial/scala/products/build.sbt
@@ -7,7 +7,7 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq(
-    "net.sf.barcode4j" % "barcode4j" % "2.0",
+    "net.sf.barcode4j" % "barcode4j" % "2.0" ,
   jdbc,
   anorm,
   cache,
