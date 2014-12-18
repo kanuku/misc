309156a088333cfd4923c3d0d765a54242bb3bf4
Fernando Benjamin <fernando.benjamin@zalando.de>
2014-12-18 23:53:55 +0100

    commiting databse scripts

--- tutorial/scala/products/conf/evolutions/default/1.sql
+++ tutorial/scala/products/conf/evolutions/default/1.sql
@@ -1,29 +1,28 @@
-#########################
+# Schemas
 # --- !Ups
-#########################
 
 CREATE TABLE products (
-    id long,
-    ean long,
+    id serial PRIMARY KEY,
+    ean bigint ,
     name varchar,
     description varchar);
 
 
 CREATE TABLE warehouses (
-    id long,
+    id serial PRIMARY KEY,
     name varchar);
     
 
 CREATE TABLE stock_items (
-    id long,
-    product_id long,
-    warehouse_id long,
-    quantity long);
+    id serial PRIMARY KEY,
+    product_id bigint,
+    warehouse_id bigint,
+    quantity smallint);
 
 
-#########################
+
 # --- !Downs
-#########################
+
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS warehouses;
 DROP TABLE IF EXISTS stock_items;
